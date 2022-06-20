f06248baa245523aa493fdef2a3a529cbec753d5
PaulTodescu <paul.todescu00@e-uvt.ro>
2022-06-20 08:49:16 +0300

    added-files

--- src/main/java/com/travelTim/food/FoodOfferDAO.java
+++ src/main/java/com/travelTim/food/FoodOfferDAO.java
@@ -5,6 +5,7 @@ import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
@@ -16,6 +17,11 @@ public interface FoodOfferDAO extends JpaRepository<FoodOfferEntity, Long> {
 //    void deleteFoodOfferEntityById(Long id);
 
     @Modifying
-    @Query("delete from FoodOfferEntity o where o.id=:id")
+    @Query("delete from FoodOfferEntity offer where offer.id = :id")
     void deleteFoodOfferEntityById(@Param("id") Long id);
+
+    @Modifying
+    @Transactional
+    @Query(value = "UPDATE FoodOfferEntity offer SET offer.email = :email, offer.phoneNumber = :phoneNumber WHERE offer.id = :id")
+    void setContact(@Param("email") String email, @Param("phoneNumber") String phoneNumber, @Param("id") Long id);
 }
